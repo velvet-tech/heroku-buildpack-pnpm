9def90d94904f0880acc1d3550b9d96f60c73ec2
Raimundo Onetto <raimille1@gmail.com>
2022-05-19 20:24:17 -0300

    now properly set env var

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -146,7 +146,7 @@ pnpm_node_modules() {
 
   echo "Installing node modules (pnpm-lock.yaml)"
   cd "$build_dir" || return
-  monitor "pnpm-install" NODE_ENV=dev; pnpm install
+  NODE_ENV=dev monitor "pnpm-install" pnpm install
 }
 
 yarn_2_install() {
